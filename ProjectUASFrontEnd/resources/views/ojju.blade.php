49e72256c5d4edf9d89803d40881ec67f7543e07
Abyys24 <masterdoesx@gmail.com>
2024-11-28 10:50:08 +0700

    mengubah navbar menjadi angular

--- ProjectUASFrontEnd/resources/views/ojju.blade.php
+++ ProjectUASFrontEnd/resources/views/ojju.blade.php
@@ -9,24 +9,7 @@
 </head>
 <body>
   <!-- Navigation Bar -->
-  <div class="container">
-    <nav class="main-nav">
-        <img src="/img/logo.png" alt="logo" class="logo">
-        <ul class="main-menu">
-          <li><a href="/">Home</a></li>
-            <li class="dropdown">
-                <a href="#">Restaurants</a>
-                <ul class="dropdown-menu">
-                    <li><a href="altitude">Altitude Grill</a></li>
-                    <li><a href="isshin">Isshin</a></li>
-                    <li><a href="pierre">Pierre</a></li>
-                    <!-- Tambah link restoran lainnya di sini -->
-                </ul>
-            </li>
-            <li><a href="about">About Bite of Bliss</a></li>
-            <li><a href="#">Contact Us</a></li>
-        </ul>
-    </nav>
+  @include('partials.navbar')
 
     <!-- Hero Section -->
     <header id="hero-section" class="hero">
