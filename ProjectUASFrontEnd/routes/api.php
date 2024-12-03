abb160944f348c4ced3a41c76defce613c2bc4b9
Abyys24 <masterdoesx@gmail.com>
2024-12-04 01:21:53 +0700

    menghilangkan change password

--- ProjectUASFrontEnd/routes/api.php
+++ ProjectUASFrontEnd/routes/api.php
@@ -5,4 +5,3 @@ use App\Http\Controllers\AuthController;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout']);
-Route::post('change-password', [AuthController::class, 'changePassword']);
